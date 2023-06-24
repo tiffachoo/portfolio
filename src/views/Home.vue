592475197bce9a923a50ef4ceb8bb2c7df3c43e6
Tiffany Choong <tiff.a.choong@gmail.com>
2023-06-24 16:59:29 -0400

    🛠️ Wip, layout updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,53 @@
 <template>
-	<header class="tc-header">
-		<h1>
-			Tiff
-		</h1>
-	</header>
-	<section class="tc-section">
-		<header>
-			<h2>
-				Work
-			</h2>
-		</header>
-		<div class="tc-grid">
-			<article 
-				v-for="item in works"
-				:key="item.title"
-				class="tc-grid-card"
-			>
-				<h3>
-					{{ item.title }}
-				</h3>
-			</article>
+	<div class="tc-image">
+		<div class="tc-image-wrap">
+			{ image goes here ? }
 		</div>
-	</section>
+	</div>
+	<div class="tc-content">
+		<section 
+			id="about"
+			class="tc-section"
+		>
+			<h1>
+				Tiffany Choong
+			</h1>
+			<p>
+				Front end developer + designer
+			</p>
+			<p>
+				{ insert bio here ? }
+				Hello! 👋
+			</p>
+			<p>
+				{ insert skills here ? }
+			</p>
+		</section>
+		<section 
+			id="Work"
+			class="tc-section"
+		>
+			<header>
+				<h2>
+					Work
+				</h2>
+			</header>
+			<ul class="tc-list">
+				<li 
+					v-for="item in works"
+					:key="item.title"
+					class="tc-list-item"
+				>
+					<router-link 
+						class="tc-list-link"
+						to="/"
+					>
+						{{ item.title }}
+					</router-link>
+				</li>
+			</ul>
+		</section>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -32,5 +58,21 @@ const works = ref(workData);
 </script>
 
 <style lang="scss">
+.tc {
+	&-list {
+		&-link {
+			text-decoration: none;
+			color: var(--color-font);
+		}
 
+		&-item {
+			padding: var(--spacer-2) 0;
+			border-bottom: 1px solid var(--color-font);
+
+			&:first-child {
+				border-top: 1px solid var(--color-font);
+			}
+		}
+	}
+}
 </style>
